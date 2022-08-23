4975c92c13986c9b0ca06d73bf6ff5ca082edcb7
ChoiJeongHyun-genesislab <80241633+ChoiJeongHyun-genesislab@users.noreply.github.com>
2022-08-23 17:10:38 +0900

    gradle 설정 변경 및 추가_v2

--- app/src/main/java/ai/genesislab/samplelibrary/MainActivity.kt
+++ app/src/main/java/ai/genesislab/samplelibrary/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        ToastLib.sShow(this, "efrgaegerg")
         sShow(this , "hi")
 
 
